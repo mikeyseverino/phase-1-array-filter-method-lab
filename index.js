c2a74b4a531c2f2dee92b01586744b618e3d9243
Michael Severino <mikeyseverino@gmail.com>
2022-02-07 21:25:22 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(driverArray, names){
+    return driverArray.filter(
+        (driverMatch) => driverMatch.toLowerCase() === names.toLowerCase()
+    );
+}
+function fuzzyMatch(driverArray, names){
+    return driverArray.filter(
+        (letterMatch) => letterMatch.charAt(0) === names.charAt(0)
+    );
+}
+function matchName(driverArray, hometown){
+    return driverArray.filter((record) => record.name === hometown);
+}
